10ee5705bdd30c001590e9fd9c192dd191da34ea
AIT-LAHCEN-REDOUAN <93483454+AIT-LAHCEN-REDOUAN@users.noreply.github.com>
2024-03-12 07:48:36 -0700

    Repository Methods

--- src/Controller/LuckyNumberController.php
+++ src/Controller/LuckyNumberController.php
@@ -2,20 +2,82 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class LuckyNumberController extends AbstractController
 {
-    #[Route('/lucky', name: 'app_lucky_number')]
-    public function index(): Response
-    {
-        $number = random_int(0,1000);
-        return $this->render('lucky_number/index.html.twig', [
-            'LuckyNumber' => $number,
-        ]);
+
+    private $em ;
+    public function __construct(EntityManagerInterface $em){
+          $this->em = $em ;
     }
+
+    //findAll Method
+    #[Route('/findAll', name: 'findAll')]
+    public function findAll(): Response
+    {
+        $repository = $this->em->getRepository(Movie::class);
+        $movie =  $repository->findAll() ;
+        dd($movie);
+    }
+
+    //find Method
+    #[Route("/find",name: "find")]
+    public function find(): Response
+    {
+        $repository = $this->em->getRepository(Movie::class);
+        $movie =  $repository->find(5) ;
+        dd($movie);
+    }
+
+
+    //findBy Method
+    #[Route("/findBy",name: "findBy")]
+    public function findBy(){
+        $repository = $this->em->getRepository(Movie::class);
+        $movie = $repository->findBy([],["id"=>'DESC']);
+        dd($movie);
+    }
+
+
+    //findOneBy Method
+    #[Route("/findOneBy",name: "findOneBy")]
+    public function findOneBy(){
+        $repository = $this->em->getRepository(Movie::class);
+        $movie = $repository->findOneBy(['id'=>6,'Title'=>"ONE PIECE1"],['id'=>"DESC"]);
+        dd($movie);
+    }
+
+    //Count Method
+    #[Route("/Count",name: "Count")]
+    public function Count(){
+        $repository = $this->em->getRepository(Movie::class);
+        $movie = $repository->count(['id'=>5]);
+        dd($movie);
+    }
+
+    //getClass Name Method
+    #[Route("/getClassName",name: "getClassName")]
+    public function getClassName(){
+        $repository = $this->em->getRepository(Movie::class);
+        $movie = $repository->getClassName();
+        dd($movie);
+    }
+
+
+
+
+
+
+
+
+
 
     #[Route('/api/posts/{id}',name: "Number",defaults: ["id"=>0],methods:['GET','HEAD'],condition: "params['id']<=1000")]
 public function show(int $id) :Response {
--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,24 +2,18 @@
 
 namespace App\Controller;
 
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MoviesController extends AbstractController
 {
-    #[Route('/movies', name: 'app_movies',/*condition: "params['name']=='ENGLAND'"*/)]
-    public function index(string $name): Response
+    #[Route('/AllMovie', name: 'app_movies')]
+    public function index(MovieRepository $movieRepository): Response
     {
-
-        //return $this->json(["message" => "Hello World"]);
-        return $this->render('movies/index.html.twig', ['name' => $name]);
-    }
-
-
-    #[Route(path: '/hello', name: 'hello')]
-    public function create(): string
-    {
-        return "hello world" ;
+        $movies = $movieRepository->findAll();
+        dd($movies);
+        //return $this->render('movies/index.html.twig', ['movie' =>$movies ]);
     }
 }
